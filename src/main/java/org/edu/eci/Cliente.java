6b3fb2d80dee65241804df956ffa75006bdcc160
andres97 <anflo.97@gmail.com>
2019-02-10 20:31:17 -0500

    Cliente

---
+++ src/main/java/org/edu/eci/Cliente.java
@@ -0,0 +1,45 @@
+package org.edu.eci;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.*;
+import java.io.*;
+
+//URL https://spark-simulacro.herokuapp.com/ la IP es 34.203.183.13
+public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Socket clienteSocket = null;
+		final int PORT = SparkWeb.getPort();
+		final String HOST = "34.203.183.13";
+		BufferedReader in;
+		PrintWriter out;
+		try {
+			in = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+			out = new PrintWriter(clienteSocket.getOutputStream());
+			clienteSocket = new Socket(HOST, PORT);
+
+			String mensaje;
+			while ((mensaje = in.readLine()) != null) {
+
+				System.out.println(mensaje);
+				if (!in.ready())
+					break;
+
+			}
+			
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
